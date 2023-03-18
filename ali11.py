770187bf360044378a5dc7de9296f77713aa3ca3
syedalihader <syedalihader03@gmail.com>
2023-03-18 16:14:08 +0500

    mycomment

---
+++ ali11.py
@@ -0,0 +1,17 @@
+a= "************************colour names**********************************"
+b = "red"
+c = "yellow"
+d = "green "
+e = "orange "
+f = "brown "
+g = "pink"
+y = "yellow"
+print(  a)
+print( " this colour is " ,b )
+print( " this colour is " ,c )
+print( " this colour is " ,d )
+print( " this colour is " ,e )
+print( " this colour is " ,f )
+print( " this colour is " ,g )
+print( " this colour is " ,y )
+
