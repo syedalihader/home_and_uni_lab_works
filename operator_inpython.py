5a47ae2e7b57caf25a2280a6134efa46419fe26c
syedalihader <syedalihader03@gmail.com>
2023-03-19 20:13:50 +0500

    todaywork18-3sunday

---
+++ operator_inpython.py
@@ -0,0 +1,49 @@
+#a = '''syed ali haider'''
+#b = "assignment"
+#c = 3378
+#d= 55.9
+
+a = 6
+b = 9
+
+# earthmatic opearators
+ 
+print("the value of 6+9 is:", 6+9)
+print("the value of 6*9 is:", 6*9)
+print("the value of 6-9 is:", 6-9)
+print("the value of 6/9 is:", 6/9)
+print("the value of 6%9 is:", 6%9)
+
+# Assignment operators ;
+
+a = 44
+a  += 5
+a  -= 34
+a /= 12
+
+print(a)
+
+
+#Comperision operaators 
+a = ( 7>9)
+
+print(a)
+
+b = ( 7<9)
+
+print(b)
+
+
+
+#type casting==> change one data type into many
+
+
+a = "232434" # string
+#a = int (a) # convert into integer 
+print( type(a))
+
+#print(a + 5)
+
+
+ 
+
