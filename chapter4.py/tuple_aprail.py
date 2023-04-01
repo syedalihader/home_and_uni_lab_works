345989fbcd0db930e8cca17817c7f7ee9b13de06
syedalihader <syedalihader03@gmail.com>
2023-04-01 13:08:51 +0500

    tuple

---
+++ chapter4.py/tuple_aprail.py
@@ -0,0 +1,16 @@
+'''#creating a tuple by ()
+t=[1,2,4,35,7,6,]
+#print the element of atuple
+print(t[0])
+#cannot update the value of a tuple
+t[0] = 34
+t1 = (1) #rong way to decleared empty tuple
+t1 = (1,) #right way tuple
+
+print(t1)
+  '''
+  
+  # new tuple methods 
+t= [ 1,2,3,4,5,6,7,8,9,]
+print(t.count(1))
+print(t.index(5))
