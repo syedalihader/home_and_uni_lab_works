9e3d169ee449b5af94dee93128190b1af59d0f89
syedalihader <syedalihader03@gmail.com>
2023-04-01 12:53:06 +0500

    list.pynew

---
+++ chapter4.py/list_and_tuplr.py
@@ -0,0 +1,25 @@
+'''a = [1,45,3,7,9]
+print(a)
+a[0]=34
+a[1]=54
+a[2]=78
+print(a[0:4])
+
+c= [45,'haider',True,8.0]
+print(c)
+c=c.count("a")
+print(c)
+#list slicing #######
+
+friends=['faraz','ali','akber','yaqoob','uzma','ayesha']
+print(friends[0:4])
+print(friends[-4:-1])'''
+l1= [1,21,3,44,5,6,78,8,9]
+#l1_sorted= l1.sort()
+
+#l1.reverse()
+#l1.append(99)
+#l1.insert(3,344)
+#l1.pop(3)
+l1.remove(78)
+print(l1)
