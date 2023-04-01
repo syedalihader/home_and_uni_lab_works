cb1d0c9969bc95f88440e51fdb1d6e4c2fa8b9e9
syedalihader <syedalihader03@gmail.com>
2023-04-01 11:51:22 +0500

    strnew

--- ifelse.py
+++ ifelse.py
@@ -2,7 +2,7 @@ t1 = ('Robert', 'Carlos','1965','Terminator 1995', 'Actor','Florida')
 t2 = (1,2,3,4,5,6,7)
 print  (t1[0])
 print  (t2[4:7])
-
+'''
 x = ("haider101", 20, "education")    
 (company, emp, profile) = x    
 print (company)
@@ -21,7 +21,8 @@ b=(5,4)
 if (a>b):print ("a is bigger")
 else: print ("b is bigger")
 
-#case 3
+#case 3'''
+
 a=(5,6)
 b=(6,4)
 if (a>b):print ("a is bigger")
---
+++ input_functions.py
@@ -0,0 +1,5 @@
+ 
+a = input(  "enter your name ")
+print(a)
+
+
