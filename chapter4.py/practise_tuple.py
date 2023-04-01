8d40494943dd2e2708ce710d703bc3cbe0eae88c
syedalihader <syedalihader03@gmail.com>
2023-04-01 14:19:36 +0500

    practise_chp_4

---
+++ chapter4.py/practise_tuple.py
@@ -0,0 +1,37 @@
+'''m1=int( input("enter fruit Number 1")
+f2= 2)nput("enter fruit Number 2")
+f3= 3nput("enter fruit Number 3")
+f4= 4nput("enter fruit Number 4")
+f5= 5iput("enter fruit Number 5")
+f6= 6nput("enter fruit Number 6")
+f7= 7put("enter fruit Number 7")
+
+myfruitslist= [f1,f2,f3,f4,f5,f6,f7]
+print(myfruitslist)
+
+#answer 2
+m1=int( input("enter marks for student  1"))
+m2=int( input("enter marks for student  2"))
+m3=int( input("enter marks for student  3"))
+m4=int( input("enter marks for student  4"))
+m5=int( input("enter marks for student  5"))
+m6=int( input("enter marks for student  6"))
+m7=int( input("enter marks for student  7"))
+ 
+
+
+marks=[m1,m2,m3,m4,m5,m6,m7]
+print(marks)'''
+
+#answer 3
+
+
+#a= [2,3,65,7]
+#print(a[0] + a[1] + a[2] + a[3])
+#print(sum(a)) 
+
+#answer 4 
+
+'''t= [1,2,4,6,3,6,9,0,6,9,0,7,0,7,0,67,0]
+t=t.count(0)
+print(t)'''
