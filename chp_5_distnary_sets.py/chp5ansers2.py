0db39e5804a877542495de2f68352c4352acee30
syedalihader <syedalihader03@gmail.com>
2023-04-02 18:10:04 +0500

    practise_setschp5

---
+++ chp_5_distnary_sets.py/chp5ansers2.py
@@ -0,0 +1,19 @@
+'''num1 = int(input("enter no 1\n"))
+num2 = int(input("enter no 2\n"))
+num3 = int(input("enter no 3\n"))
+num4 = int(input("enter no 4\n"))
+num5 = int(input("enter no 5\n"))
+num6 = int(input("enter no 6\n"))
+num7 = int(input("enter no 7\n"))
+num8 = int(input("enter no 8\n"))
+num9 = int(input("enter no 9\n")) 
+
+s= {num1,num2,num3,num4,num5,num6,num7,num8,num9}
+print(s)
+'''
+
+#answer 3
+
+s= {19,"19"}
+print(s)
+#cannot run 
