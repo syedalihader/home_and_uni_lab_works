9c30d6667f73f6550507b76bb011ab7a554aebed
syedalihader <syedalihader03@gmail.com>
2023-04-08 20:50:52 +0500

    2

---
+++ Assignments.py/linked_list.py
@@ -0,0 +1,45 @@
+class node:
+  def __init__(self, data=None, next=None):
+    self.data = data      
+    self.next = next
+          
+class Linkedlist:
+    def __init__(self) :
+     self.head = None
+         
+def insert_at_begining(self, data):
+    node = node(data,self.head)
+    self.head = node
+    
+    
+def print(self):
+    if self.head is None:
+         print("linked list is empty")
+         return  
+         
+    itr= self.head        
+    listr = ''
+
+
+
+    while itr:
+        listr +=str(itr.data)
+        itr= itr.next
+      
+print(Linkedlist)
+
+
+
+
+    
+if  __name__=='__main__'  : 
+    li=Linkedlist()
+    li.insert_at_end(56)   
+    li.insert_at_end(66)
+    li.print()   
+
+
+    
+    
+                    
+          
