3aa4884f3ac2d24338710857fccc39b3ebe416e0
syedalihader <syedalihader03@gmail.com>
2023-04-08 17:28:38 +0500

    stack and queue

---
+++ Assignments.py/stack.py
@@ -0,0 +1,37 @@
+ # stack using list
+stack = ["hababi", "dubai", "lahore"]
+stack.append("Ramazan")
+stack.append( "koita")
+stack.append("hafiz")
+stack.append("hafiz")
+stack.append("hamza")
+stack.append("Iqra")
+stack.append("I2")
+stack.append("I4")
+stack.append("I5l")
+stack.append("I5bl")
+stack.append("I5bl")
+print(stack)
+print(stack.pop())
+print(stack)
+
+
+queue = ["syed", "Akbar", "ali"]
+queue.append("Raju")
+queue.append("kamran")
+queue.append("kam")
+queue.append("kran")
+queue.append("kan")
+queue.append("mran")
+queue.append("an")
+print(queue)
+  
+# Removes the first item
+print(queue.pop(-5))
+  
+print(queue)
+  
+# Removes the first item
+print(queue.pop(4))
+  
+print(queue)
