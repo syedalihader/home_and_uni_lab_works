0aefb831e0aa0f59234cab7960b4beae17e7c2f0
syedalihader <syedalihader03@gmail.com>
2023-04-08 16:34:22 +0500

    1

---
+++ data_structure_and_algorithms.py/time.complexity.py
@@ -0,0 +1,19 @@
+'''import time
+
+start = time.time()
+for i in range [1101]:
+{
+print(i)
+}
+print(time.time()-start)'''
+
+
+import time
+
+start = time.time()
+i =1
+while (i<101):
+print(i)
+i+ =1
+print(time.time()-start)
+    
